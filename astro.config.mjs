c49bc06dca6d51ba2df190a15cdcaff432400c43
unknown <a.elswefy@hawsabah.sa>
2024-10-13 14:21:30 +0300

    hh

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,17 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    build: {
+  base: './',
+  trailingSlash: 'ignore',
+  build: {
+    // distDir: 'dist',
+    assets: 'dist',
     format: 'file',  // This ensures separate folders for each page
     outDir: 'dist',       // Output directory
-    inlineStylesheets: 'never', 
-    minify: false
-  }});
+    inlineStylesheets: 'never',
+    minify: false,
+
+  },
+});
+
+
